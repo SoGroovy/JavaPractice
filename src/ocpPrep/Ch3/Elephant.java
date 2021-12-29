b30576c2b663c9fef825b15ebef126332f20ee59
Tin Phan <tinphan09@gmail.com>
2021-12-29 14:10:17 -0600

    Elephant

--- src/ocpPrep/Ch3/Elephant.java
+++ src/ocpPrep/Ch3/Elephant.java
@@ -1,2 +1,11 @@
-package ocpPrep.Ch3;public class Elephant {
+package ocpPrep.Ch3;
+
+//Crate class can deal with Elephant without knowing anything about it
+public class Elephant {
+    Elephant elephant = new Elephant();
+    Crate<Elephant> crateForElephant = new Crate<>();
+    crateForElephant.packCrate(elephant);
+    Elephant inNewHome = crateForElephant.emptyCrate();
+
+    Crate<Zebra> crateForZebra = new Crate<>();
 }
