3c1836ca3c084fd7aaa86dfcefd7f954e59880bb
Tin Phan <tinphan09@gmail.com>
2021-12-29 14:09:38 -0600

    Refactored name to Crate

--- src/ocpPrep/Ch3/GenericClasses.java
+++ src/ocpPrep/Ch3/GenericClasses.java
@@ -1,8 +1,9 @@
 package ocpPrep.Ch3;
 
+//GenericClasses
 //Generic type T is available anywhere within the class
 //Naming conventions for generics is UPPERCASE SINGLE letters
-public class GenericClasses <T>{
+public class Crate <T>{
     private T contents;
     public T emptyCrate() {
         return contents;
