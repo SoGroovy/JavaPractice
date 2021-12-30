9e53dc658fa33fa9cdbee50f602948feaa39f421
Tin Phan <tinphan09@gmail.com>
2021-12-29 22:19:52 -0600

    Final Revision

--- src/AlgorithmicToolBox/Week3/FractionalKnapsack.java
+++ src/AlgorithmicToolBox/Week3/FractionalKnapsack.java
@@ -1,4 +1,5 @@
 package AlgorithmicToolBox.Week3;
+
 import java.util.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -6,31 +7,36 @@ import java.util.List;
 public class FractionalKnapsack {
     private static double getOptimalValue(int capacity, int[] values, int[] weights) {
         double maxLoot = 0.0;
-        int m = 0;
-        List<Integer> valuesL = new ArrayList<Integer>(values.length);
-        for (int i = 0; i < values.length; i++){
+
+        List<Integer> valuesL = new ArrayList<>(values.length);
+        for (int i = 0; i < values.length; i++) {
             valuesL.add(values[i]);
         }
 
-        List<Integer> weightsL = new ArrayList<Integer>(weights.length);
-        for (int i = 0; i < weights.length; i++){
+        List<Integer> weightsL = new ArrayList<>(weights.length);
+        for (int i = 0; i < weights.length; i++) {
             weightsL.add(weights[i]);
         }
         //creating return condition where there is no capacity or there are no items
         if (capacity == 0 || weightsL.size() == 0) return 0.0;
+
         while (capacity > 0) {
+            int m = 0;
+            if (values.length == 1) {
+                int amount = Math.min(weightsL.get(m), capacity);
+                return maxLoot = maxLoot + ((double) valuesL.get(m) * amount) / weightsL.get(m);
+            }
             for (int i = 1; i < weightsL.size(); i++) {
-                if(valuesL.get(i) * weightsL.get(m) > valuesL.get(m) * weightsL.get(i)) {
+                if (valuesL.get(i) * weightsL.get(m) > valuesL.get(m) * weightsL.get(i)) {
                     m = i;
                 }
             }
             int amount = Math.min(weightsL.get(m), capacity);
-            maxLoot = maxLoot + ((double)valuesL.get(m) * amount) / weightsL.get(m);
+            maxLoot = maxLoot + ((double) valuesL.get(m) * amount) / weightsL.get(m);
 
             valuesL.remove(m);
             weightsL.remove(m);
             capacity -= amount;
-            m = 0;
         }
 
         return maxLoot;
