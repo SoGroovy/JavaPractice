1a2d46d2883c4d96e90cad6101375e055a2edd91
Tin Phan <tinphan09@gmail.com>
2021-12-30 00:49:19 -0600

    isIsomorphic

---
+++ src/LeetCode/Easy/IsoMorphicStrings.java
@@ -0,0 +1,74 @@
+package LeetCode.Easy;
+
+/*
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters.
+No two characters may map to the same character, but a character may map to itself.
+ */
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsoMorphicStrings {
+    public static void main(String[] args) {
+        testisIsomorphic();
+        testisIsomorphic1();
+        testisIsomorphic2();
+        testisIsomorphic3();
+    }
+
+    public static boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> charMapF = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            charMapF.put(s.charAt(i), t.charAt(i));
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (charMapF.get(s.charAt(i)) != t.charAt(i)) {
+                return false;
+            }
+        }
+        Map<Character, Character> charMapB = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            charMapB.put(t.charAt(i), s.charAt(i));
+        }
+        for (int i = 0; i < t.length(); i++) {
+            if (charMapB.get(t.charAt(i)) != s.charAt(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void testisIsomorphic() {
+        String s = "egg";
+        String t = "add";
+        System.out.println("Actual: " + isIsomorphic(s, t));
+        System.out.println("Expected: true");
+    }
+
+    public static void testisIsomorphic1() {
+        String s = "foo";
+        String t = "bar";
+        System.out.println("Actual: " + isIsomorphic(s, t));
+        System.out.println("Expected: false");
+    }
+
+    public static void testisIsomorphic2() {
+        String s = "paper";
+        String t = "title";
+        System.out.println("Actual: " + isIsomorphic(s, t));
+        System.out.println("Expected: true");
+    }
+
+    public static void testisIsomorphic3() {
+        String s = "babc";
+        String t = "baba";
+        System.out.println("Actual: " + isIsomorphic(s, t));
+        System.out.println("Expected: false");
+    }
+}
