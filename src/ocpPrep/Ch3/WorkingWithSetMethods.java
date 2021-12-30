4d7ac73d82fd3e40c772197662ae9186567eb05d
Tin Phan <tinphan09@gmail.com>
2021-12-29 22:34:42 -0600

    SetMethods

---
+++ src/ocpPrep/Ch3/WorkingWithSetMethods.java
@@ -0,0 +1,29 @@
+package ocpPrep.Ch3;
+
+import com.sun.source.tree.Tree;
+
+import java.util.HashSet;
+import java.util.NavigableSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+public class WorkingWithSetMethods {
+    public static void main(String[] args) {
+//        Set<Integer> set = new HashSet<>();
+//        boolean b1 = set.add(66);                       //true
+//        boolean b2 = set.add(10);                       //true
+//        boolean b3 = set.add(66);                       //false, showing you can't add duplicates to set
+//        boolean b4 = set.add(8);                        //true
+//        for(Integer integer: set) {
+//            System.out.println(integer + ",");          //66, 8, 10 | this is using a HashSet
+//        }                                               //8, 10, 66 | this is using a TreeSet
+//
+//        NavigableSet<Integer> set1 = new TreeSet<>();
+//        for (int i = 0; i <= 20; i++) set.add(i);
+//        System.out.println(set.lower(10));              //9
+//        System.out.println(set.floor(10));              //10
+//        System.out.println(set.ceiling(20));            //20
+//        System.out.println(set.higher(20));             //null
+    }
+}
