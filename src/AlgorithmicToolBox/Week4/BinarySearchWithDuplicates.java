755916b695fc0cd102b74e36f860c51872ee0053
Tin Phan <tinphan09@gmail.com>
2022-01-07 02:41:52 -0600

    fast

--- src/AlgorithmicToolBox/Week4/BinarySearchWithDuplicates.java
+++ src/AlgorithmicToolBox/Week4/BinarySearchWithDuplicates.java
@@ -1,2 +1,84 @@
-package AlgorithmicToolBox.Week4;public class BinarySearchWithDuplicates {
+package AlgorithmicToolBox.Week4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BinarySearchWithDuplicates {
+    public static int binarySearch(int[] a, int x) {
+        int left = 0, right = a.length - 1, result = - 1;
+        while (right >= left){
+            int mid = (left + right) / 2;
+            if (a[mid] == x) {
+//                int top = mid;                slow with many duplicates
+//                while (top > 0 && a[top] == a[top - 1]){
+//                    top = top - 1;
+//                }
+//                return top;
+                  right = mid - 1;
+                  result = mid;
+            } else if (a[mid] < x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    static int linearSearch(int[] a, int x) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == x) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        int m = scanner.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            b[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < m; i++) {
+            System.out.print(binarySearch(a, b[i]) + " ");
+//            System.out.print(linearSearch(a, b[i]) + " ");
+        }
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
 }
