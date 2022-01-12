a90939d0781c99f36cbd5900264b4acb954886f5
Tin Phan <tinphan09@gmail.com>
2022-01-11 23:56:47 -0600

    revised

--- src/ArraysListsData/Week1/CaesarCipher.java
+++ src/ArraysListsData/Week1/CaesarCipher.java
@@ -1,7 +1,7 @@
 package ArraysListsData.Week1;
 
 public class CaesarCipher {
-    public String encrypt (String input, int key) {
+    public String encrypt(String input, int key) {
         String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String shiftedAlpha = alphabet.substring(key) + alphabet.substring(0, key);
         StringBuilder encryptedM = new StringBuilder(input);
@@ -11,8 +11,8 @@ public class CaesarCipher {
 
             if (idx != -1) {
                 char newChar = shiftedAlpha.charAt(idx);
-                if(Character.isLowerCase(currentChar)) newChar = Character.toLowerCase(newChar);
-                if(Character.isUpperCase(currentChar)) newChar = Character.toUpperCase(newChar);
+                if (Character.isLowerCase(currentChar)) newChar = Character.toLowerCase(newChar);
+                if (Character.isUpperCase(currentChar)) newChar = Character.toUpperCase(newChar);
                 encryptedM.setCharAt(i, newChar);
             }
         }
@@ -20,56 +20,45 @@ public class CaesarCipher {
     }
 
 //    public void testEncrypt() {
-//        int key = 17;
+//        int key = 15;
 //        FileResource fr = new FileResource();
 //        String message = fr.asString();
 //        String encrypted = encrypt(message, key);
 //        System.out.println(encrypted);
-//        String decrypted = encrypt(encrypted, 26-key);
+//        String decrypted = encrypt(encrypted, 26 - key);
 //        System.out.println(decrypted);
 //
 //        String message1 = "First Legion";
 //        int key1 = 23;
 //        String encrypted1 = encrypt(message1, key1);
 //        System.out.println(encrypted1);
-//        String decrypted1 = encrypt(encrypted1, 26-key1);
+//        String decrypted1 = encrypt(encrypted1, 26 - key1);
 //        System.out.println(decrypted1);
 //    }
 
-    public String encryptTwoKeys (String input, int key1, int key2) {
-        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String shiftedA = alphabet.substring(key1) + alphabet.substring(0, key1);
-        String shiftedB = alphabet.substring(key2) + alphabet.substring(0, key2);
+    public String encryptTwoKeys(String input, int key1, int key2) {
+        String shiftedA = encrypt(input, key1);
+        String shiftedB = encrypt(input, key2);
         StringBuilder newPhrase = new StringBuilder(input);
 
         for (int i = 0; i < input.length(); i++) {
-            char currentChar = newPhrase.charAt(i);
-            int currentIndex = alphabet.indexOf(Character.toUpperCase(currentChar));
-
-            if (currentIndex != -1) {
-                if (i % 2 == 0){
-                    char newChar = shiftedA.charAt(currentIndex);
-                    if (Character.isLowerCase(currentChar)) newChar = Character.toLowerCase(shiftedA.charAt(currentIndex));
-                    if (Character.isUpperCase(currentChar)) newChar = Character.toUpperCase(shiftedA.charAt(currentIndex));
-                    newPhrase.setCharAt(i, newChar);
-                } else {
-                    char newChar = shiftedB.charAt(currentIndex);
-                    if (Character.isLowerCase(currentChar)) newChar = Character.toLowerCase(shiftedB.charAt(currentIndex));
-                    if (Character.isUpperCase(currentChar)) newChar = Character.toUpperCase(shiftedB.charAt(currentIndex));
-                    newPhrase.setCharAt(i, newChar);
-                }
+            if (i % 2 == 0) {
+                newPhrase.setCharAt(i, shiftedA.charAt(i));
+            } else {
+                newPhrase.setCharAt(i, shiftedB.charAt(i));
             }
         }
         return newPhrase.toString();
     }
 
 //    public void testEncryptTwoKeys() {
-//        int key1 = 23, key2 = 17;
+//        int key1 = 8, key2 = 21;
 //        FileResource fr = new FileResource();
 //        String message = fr.asString();
 //        String encrypted = encryptTwoKeys(message, key1, key2);
 //        System.out.println(encrypted);
-//        String decrypted = encryptTwoKeys(encrypted, 26-key1, 26-key2);
+//        String decrypted = encryptTwoKeys(encrypted, 26 - key1, 26 - key2);
 //        System.out.println(decrypted);
 //    }
 }
+
