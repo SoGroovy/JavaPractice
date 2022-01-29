8b4cdfc6ed8d9d7bfb116e77fba424d37692d26b
Tin Phan <tinphan09@gmail.com>
2022-01-28 21:29:03 -0600

    return type

---
+++ src/OCAPrep/Ch4/ReturnType.java
@@ -0,0 +1,21 @@
+package OCAPrep.Ch4;
+
+public class ReturnType {
+    /*
+    public void walk1() {}
+    public void walk2() {return;}
+    public String walk3() {return "";}
+    public String walk4() {} (DOES NOT COMPILE because does it returns null)
+    public walk5() {} (DOES NOT COMPILE because there is no return type)
+    String walk6() {if(a == 4) return ""; } (DOES NOT COMPILE because it returns a string on a condition; if return type is declared, it always needs to be returned)
+
+     int integer() {
+        return 9;
+     }
+
+     int long() {
+        return 9L;  //DOES NOT COMPILE because you cannot store a long in an integer variable
+     }
+
+     */
+}
