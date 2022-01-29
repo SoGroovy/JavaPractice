4b46235eaa2a18f372e93804653d31d355a33eac
Tin Phan <tinphan09@gmail.com>
2022-01-28 20:55:13 -0600

    designing methods

---
+++ src/OCAPrep/Ch4/DesigningMethods.java
@@ -0,0 +1,20 @@
+package OCAPrep.Ch4;
+
+public class DesigningMethods {
+        public final void nap (int minutes) throws InterruptedException {
+            /*
+            What is required for method declaration?
+            public = Access modifier (not required)
+            final = optional specifier (not required)
+            void = return type (required)
+            nap = method name (required)
+            (int minutes) = parameters (required, but can be empty)
+            throws = exception (not required)
+            {//take a nap} = method body (required, but can be empty)
+            //take a nap
+
+
+
+             */
+        }
+}
