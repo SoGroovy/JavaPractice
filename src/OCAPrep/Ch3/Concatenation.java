eaf255a599402a879dff9f58ecd304734360cd92
Tin Phan <tinphan09@gmail.com>
2022-02-10 21:20:59 -0600

    concat

--- src/OCAPrep/Ch3/Concatenation.java
+++ src/OCAPrep/Ch3/Concatenation.java
@@ -1,2 +1,9 @@
-package OCAPrep.Ch3;public class Concatenation {
+package OCAPrep.Ch3;
+
+public class Concatenation {
+    public  void methodA() {
+        System.out.println("A" + "B" + 3);              //AB3
+        System.out.println(1 + 2 + "c");                //3c
+        System.out.println(5 + 7 + "c" + false);        //12cfalse
+    }
 }
