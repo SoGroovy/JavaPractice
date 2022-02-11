7bcee2b7548b7f56d172b4eb4f7f17f902048d0b
Tin Phan <tinphan09@gmail.com>
2022-02-10 21:23:23 -0600

    rules

--- src/OCAPrep/Ch3/Main.java
+++ src/OCAPrep/Ch3/Main.java
@@ -3,6 +3,10 @@ package OCAPrep.Ch3;
 What is a String? A sequence of characters
 String is a class, but very special. It does not require new to initialize it.
 They are immutable; this means that you cannot change it. The string cannot be large or smaller. It is fixed.
+***********RULES*********
+1) If both operands are numeric, + means numeric addition; - means numeric subtraction, etc
+2) If either operand is a String, + means concatenation
+3) The expression is evaluated left to right.
  */
 public class Main {
     public static void main(String[] args) {
