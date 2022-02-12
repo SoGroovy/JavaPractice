e35dcf1bc2a28139b834825a742d8bb341caf9be
Tin Phan <tinphan09@gmail.com>
2022-02-11 23:04:25 -0600

    arrays with ref var

--- src/OCAPrep/Ch3/Arrays/ArraysWithRefVariables.java
+++ src/OCAPrep/Ch3/Arrays/ArraysWithRefVariables.java
@@ -1,2 +1,14 @@
-package OCAPrep.Ch3.Arrays;public class ArraysWithRefVariables {
+package OCAPrep.Ch3.Arrays;
+
+public class ArraysWithRefVariables {
+    /*
+    //All are legal
+    String[] strings = {"String value"};
+    Object[] objects = new String[2];                             //objects are broader type than String, no cast necessary
+    String[] againStrings = (String[]) objects;             //Since String is more specific, a cast is needed
+
+    //Not legal
+    againStrings[0] = new StringBuilder();                  //does not compile; String[] cannot store StringBuilder, only strings
+    objects[0] = new StringBuilder();                       //objects is actually a String[], so an ArrayStoreException is thrown
+     */
 }
