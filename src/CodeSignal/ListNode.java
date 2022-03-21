6fda6d23d9dd980abbbf3a0a5ba7acc774d0d6b3
Tin Phan <tinphan09@gmail.com>
2022-03-21 15:26:59 -0500

    list node code signal

--- src/CodeSignal/ListNode.java
+++ src/CodeSignal/ListNode.java
@@ -1,2 +1,34 @@
-package CodeSignal;public class IsListPalindrome {
-}
+package CodeSignal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ListNode<T> {
+        ListNode(T x) {
+            value = x;
+        }
+
+        T value;
+        ListNode<T> next;
+
+        boolean solution(ListNode<Integer> l) {
+            List<Integer> l1 = new ArrayList<>();
+            List<Integer> l2 = new ArrayList<>();
+            while(l != null) {
+                l1.add(l.value);
+                l = l.next;
+            }
+            for (int i = l1.size(); i >= 0; i--) {
+                l2.add(l1.get(i));
+            }
+
+            if(!(l1.equals(l2))) {
+                return false;
+            }
+            return true;
+        }
+
+        public static void main(String[] args) {
+
+        }
+    }
