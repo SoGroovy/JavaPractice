5fb9012e9b4483d9b8aa015efd7c7b46c0fc5301
Tin Phan <tinphan09@gmail.com>
2022-03-21 15:27:19 -0500

    remove k from list code signal

--- src/CodeSignal/RemoveKFromList.java
+++ src/CodeSignal/RemoveKFromList.java
@@ -1,2 +1,20 @@
-package CodeSignal;public class RemoveKFromList {
+package CodeSignal;
+
+public class RemoveKFromList {
+    ListNode<Integer> solution(ListNode<Integer> l, int k) {
+        ListNode<Integer> last = l;
+        ListNode<Integer> current = last;
+        while(current != null) {
+            if (current.value != k) {
+                current = current.next;
+                last = last.next;
+            } else {
+                current = current.next;
+                last.next = current;
+                last = last.next;
+            }
+        }
+
+        return l;
+    }
 }
